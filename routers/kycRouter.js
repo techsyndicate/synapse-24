d732113a806eeb6b418d1380b66a83d80a5ecece
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-04-30 20:00:05 +0530

    made status page

--- routers/kycRouter.js
+++ routers/kycRouter.js
@@ -4,29 +4,14 @@ const Driver = require('../schemas/driverSchema')
 
 
 router.get('/', async (req, res) => {
+    if (req.user.type != "Driver" || req.user.type != "driver") {
+        return res.redirect("/")
+    }
     res.render('kyc')
 })
 
 router.post('/uploadKYC', async (req, res) => {
-    // const {kycImage} = req.body
-    // console.log(kycImage)
-    // res.redirect('/kyc')
-    const reqUser = await User.findOne({email: req.user.email})
-    let name = reqUser.fname + " " + reqUser.lname
-    const newDriver = new Driver({
-        email: req.user.email,
-        name: name,
-        kyc: "true"
-     })
-     await newDriver.save()
-     console.log(newDriver)
 
-    await User.updateOne({email: req.user.email}, {
-        $set: {
-            type: "Driver"
-        }
-    })
-     res.redirect("/")
 })
 
 module.exports = router
