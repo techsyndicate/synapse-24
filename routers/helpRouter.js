64b8df63f2dd5aa8c7c93fc4ab6c9a3db7666a4e
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-05-02 20:39:32 +0530

    removed keys

--- routers/helpRouter.js
+++ routers/helpRouter.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const router = require('express').Router()
 
+
 router.get('/', (req, res) => {
-    res.render('help', {user: req.user})
+    res.render('help', {user: req.user, gapi: process.env.GOOGLE_API})
 })
 
 module.exports = router
