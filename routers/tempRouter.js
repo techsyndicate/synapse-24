2d7834920565755266a2c361afe0d8d9ad6998aa
Shyamak-Seth <sethshyamak@gmail.com>
2024-05-08 00:43:46 +0530

    bruh

--- routers/tempRouter.js
+++ routers/tempRouter.js
@@ -3,12 +3,7 @@ const Ride = require('../schemas/rideSchema')
 const User = require('../schemas/userSchema')
 
 router.get('/', async (req, res) => {
-    const foundRide = await Ride.findOne({rideId: 'xHxW6J9lIiSER5fV1Kre'})
-    await Ride.updateOne({rideId: 'xHxW6J9lIiSER5fV1Kre'}, {
-        $set: {
-            seats: ['a3']
-        }
-    })
+    res.render('temp')
 })
 
 module.exports = router
